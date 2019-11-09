d88b25258269a73f78a6f6e9a3d3727adba40e19
Zee Celest <zeecelest@gmail.com>
2019-11-09 15:38:57 -0500

    added more code

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
-//exports the app ready for integration testing
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
+const bookmarksRouter = require('./bookmarks/bookmarks-router');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption));
-app.use(helmet());
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}));
 app.use(cors());
+app.use(helmet());
+app.use(validateBearerToken);
+
+app.use(bookmarksRouter);
 
 //make test pass by adding basic endpoints to app.js
 app.get('/', (req, res) => {
@@ -22,15 +25,7 @@ app.get('/', (req, res) => {
 });
 
 //provide error messages
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
+  
 
 module.exports = app;
--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,10 @@
-const { expect } = require('chai')
-const supertest = require('supertest')
+process.env.TZ = 'UTC';
+process.env.NODE_ENV = 'test';
+
+require('dotenv').config();
+const { expect } = require('chai');
+const supertest = require('supertest');
 
 //sets 2 global variables
-global.expect = expect
-global.supertest = supertest
+global.expect = expect;
+global.supertest = supertest;
